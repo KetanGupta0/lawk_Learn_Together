453be2706b037e3f34d02ae4cfb3da7d8d2a5384
Ketan Gupta <ckg4155@gmail.com>
2024-08-30 12:21:03 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -0,0 +1,101 @@
+package com.example.learntogether
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.learntogether.ui.theme.LearnTogetherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LearnTogetherTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LearnTogetherImage(
+                        title = stringResource(R.string.title),
+                        para1 = stringResource(R.string.paragraph_1),
+                        para2 = stringResource(R.string.paragraph_2)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LearnTogetherImage(title: String, para1: String, para2: String, modifier: Modifier = Modifier){
+    val image = painterResource(R.drawable.bg_compose_background)
+    Column {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth
+        )
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(
+                top = 16.dp,
+                bottom = 16.dp,
+                start = 16.dp,
+                end = 16.dp
+            )
+        )
+        Text(
+            text = para1,
+            lineHeight = 20.sp,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(
+                start = 16.dp,
+                end = 16.dp
+            )
+        )
+        Text(
+            text = para2,
+            fontSize = 15.sp,
+            lineHeight = 20.sp,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(
+                top = 16.dp,
+                bottom = 16.dp,
+                start = 16.dp,
+                end = 16.dp
+            )
+        )
+    }
+}
+
+@Preview(showBackground = true, name = "Learn Together")
+@Composable
+fun GreetingPreview() {
+    LearnTogetherTheme {
+        LearnTogetherImage(
+            title = stringResource(R.string.title),
+            para1 = stringResource(R.string.paragraph_1),
+            para2 = stringResource(R.string.paragraph_2),
+            modifier = Modifier
+        )
+    }
+}
